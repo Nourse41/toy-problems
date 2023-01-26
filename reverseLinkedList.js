74fba2a353e7a3cddbee9b04a2b6011966291bec
Tyler Nourse <nourse41@gmail.com>
2023-01-26 12:54:26 -0500

    completed reverseLinkedList

---
+++ reverseLinkedList.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function(head) {
+  // declare previous & current pointers
+  let previous = null, current = head, temp = null;
+
+  // traverse list
+  while (current !== null) {
+    // set temp to current.next value
+    temp = current.next;
+
+    // set current nodes next value to previous pointer
+    current.next = previous;
+
+    // set the current pointer to previous
+    previous = current
+
+    // pointer current to current.next
+    current = temp;
+  }
+  return previous;
+}
