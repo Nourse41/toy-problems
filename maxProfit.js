be647e8a456923c1f31445a0fb13106d5ba7d0a8
Tyler Nourse <nourse41@gmail.com>
2023-02-15 14:34:57 -0500

    updating toy problems

---
+++ maxProfit.js
@@ -0,0 +1,64 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+Constraints:
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+// Input: takes in an array of prices
+// Output: returns highest possible profit
+// Output: returns highest possible profit
+
+var maxProfit = function(prices) {
+  let left = 0, right = 1, maxSum = 0;
+
+  while (left < prices.length - 1) {
+
+    if (right > prices.length - 1) {
+      left++
+      right = left + 1;
+     }
+
+    let currSum = prices[right] - prices[left];
+
+    if (currSum > maxSum) {
+      maxSum = currSum;
+      right++;
+    } else {
+      right++
+    }
+  }
+  return maxSum;
+};
+
+
+
+var smartMaxProfit = function(prices) {
+  let left = 0, right = 1, maxSum = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let currSum = prices[right] - prices[left];
+      maxSum = (maxSum < currSum) ? currSum : maxSum;
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return maxSum;
+};
+
+console.log(maxProfit([2,1,4]));
